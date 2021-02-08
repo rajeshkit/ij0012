5843442ea73bc9c200fb8b8a5014eb84ae903f0f
Rajesh Nallusamy <rajesh.kit@gmail.com>
2021-02-08 16:45:18 +0530

    version 2.0 added one sysout extra

--- src/ij0012/Main.java
+++ src/ij0012/Main.java
@@ -3,7 +3,8 @@ package ij0012;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Worl");
+		System.out.println("Hello World");
+		System.out.println("INTCDE21IJ001 INTCDE21IJ001");
 
 	}
 
